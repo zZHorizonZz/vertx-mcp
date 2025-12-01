cef9bc89d60e2cba238764176cdac8ed56d7c37c
Daniel Fiala <danfiala23@gmail.com>
2025-12-01 13:06:41 +0100

    Add `vertx-mcp-client` module with dependencies, build configuration, and module metadata.

---
+++ vertx-mcp-client/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module io.vertx.mcp.client {
+  requires io.vertx.jsonschema;
+  requires io.vertx.codegen.api;
+
+  requires io.vertx.codegen.json;
+  requires io.vertx.core;
+
+  requires io.vertx.mcp.common;
+  requires java.logging;
+}
